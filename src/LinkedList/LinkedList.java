9c41896674d02d0532ff6afdf7ca8475e6821eba
royrajiv <rajiv.roy1@gmail.com>
2022-04-21 23:11:48 -0400

    Update LinkedList.java

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,4 +1,22 @@
 package LinkedList;
+/*
+class Link {
+    public int iData; // data item (key)
+    public double dData; // data item
+    public Link next; // next link in list
+
+    public Link(int id, double dd) // constructor
+    {
+        iData = id; // initialize data
+        dData = dd; // (‘next’ is automatically set to null
+    }
+
+    public void displayLink() // display yourself
+    {
+        System.out.print(" {" + iData + ", " + dData + "} ");
+    }
+}
+*/
 
 public class LinkedList {
     private Link head;
